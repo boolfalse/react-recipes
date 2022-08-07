6bffedf35981c1fc5c8950e8c6049048b4f838d4
boolfalse <boolfalse@mail.ru>
2022-08-08 03:15:22 +0400

    get data from external api and refresh the useState in useEffect

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,28 @@
 
+import {useEffect, useState} from "react";
+
 const Popular = () => {
+    const [popularState, setPopularState] = useState([]);
+
+    useEffect(() => {
+        getPopular().then(r => console.log(r)).catch(e => console.log(e));
+    }, []);
+
+    const getPopular = async () => {
+        const data = await fetch(`${process.env.REACT_APP_SPOONACULAR_API_URL}/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`);
+        const result = await data.json();
+        setPopularState(result.recipes);
+    }
+
     return (
-        <div>Popular</div>
+        <div>
+            {popularState.map(recipe => (
+                <div key={recipe.id}>
+                    <h3>{recipe.title}</h3>
+                    <img src={recipe.image} alt={recipe.creditsText} />
+                </div>
+            ))}
+        </div>
     )
 }
 
